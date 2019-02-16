70b70a6253309ddcca1b46a8754bec7b5b704668
bsydenham <bjorn.sydenham@yahoo.co.uk>
2019-02-16 14:24:10 +0000

    Tidying up plots

--- spending_graph.py
+++ spending_graph.py
@@ -9,19 +9,25 @@ transactions = services.get_transactions('2018-10-01', today)
 df = pd.DataFrame(transactions)
 df['created'] = pd.to_datetime(df['created'])
 
-
 outbound_transactions = df[df.direction == 'OUTBOUND'][['created', 'amount']].sort_values('created')
 outbound_transactions['amount'] = abs(df['amount'])
-grouped_transactions_day = outbound_transactions.set_index('created').groupby(pd.Grouper(freq='D')).sum()
-grouped_transactions_month = outbound_transactions.set_index('created').groupby(pd.Grouper(freq='M')).sum()
 
-#fig, axs = plt.subplots(1, 2)
-grouped_transactions_day.plot()
-plt.ylim(0)
+grouped_transactions_day = outbound_transactions.groupby(pd.Grouper(key='created', freq='D')).sum()
+grouped_transactions_month = outbound_transactions.groupby(pd.Grouper(key='created', freq='M')).sum()
 
-grouped_transactions_month.plot.bar()
+fig, axs = plt.subplots(nrows = 1, ncols = 2)
+
+grouped_transactions_day.plot(ax=axs[0], legend=None)
+axs[0].set_title('Amount spent per day')
+axs[0].set_ylim(0)
+axs[0].set_xlabel('Date')
+axs[0].set_ylabel('Amount spent (£)')
+
+grouped_transactions_month.plot.bar(ax=axs[1], legend=None, rot=0)
+axs[1].set_title('Amount spent per month')
+axs[1].set_ylim(0)
+axs[1].set_xticklabels(grouped_transactions_month.index.strftime('%y-%b'))
+axs[1].set_xlabel('Date')
+axs[1].set_ylabel('Amount spent (£)')
 
 plt.show()
-
-# plt.plot(outbound_transactions.created, outbound_transactions.amount)
-# plt.show()
