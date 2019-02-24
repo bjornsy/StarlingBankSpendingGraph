7cdeb17b62e92c434d46fa6e5968be3a2d2c7727
bsydenham <bjorn.sydenham@yahoo.co.uk>
2019-02-24 15:46:46 +0000

    Date and amount annotation working for per day graph

--- spending_graph/graph_helpers.py
+++ spending_graph/graph_helpers.py
@@ -2,3 +2,13 @@ def set_common_properties(axs_reference: 'matplotlib.axes._subplots.AxesSubplot'
     axs_reference.set_ylim(0)
     axs_reference.set_xlabel('Date')
     axs_reference.set_ylabel('Amount spent (£)')
+
+def create_annot(axs_reference: 'matplotlib.axes._subplots.AxesSubplot') -> 'matplotlib.text.Annotation':
+    annot = axs_reference.annotate("", xy=(0,0), xytext=(-20,20),textcoords="offset points",
+                    bbox=dict(boxstyle="round", fc="w"),
+                    arrowprops=dict(arrowstyle="->"))
+    annot.set_visible(False)
+    return annot
+
+def format_currency(num: float) -> str:
+    return '{:.2f}'.format(num)
--- spending_graph/spending_graph.py
+++ spending_graph/spending_graph.py
@@ -18,7 +18,8 @@ grouped_transactions_month = outbound_transactions.groupby(pd.Grouper(key='creat
 
 fig, axs = plt.subplots(nrows = 1, ncols = 2)
 
-grouped_transactions_day.plot(ax=axs[0], legend=None)
+plot_day = grouped_transactions_day.plot(ax=axs[0], legend=None)
+line = plot_day.lines[0]
 graph_helpers.set_common_properties(axs[0])
 axs[0].set_title('Amount spent per day')
 
@@ -26,5 +27,38 @@ grouped_transactions_month.plot.bar(ax=axs[1], legend=None, rot=0)
 graph_helpers.set_common_properties(axs[1])
 axs[1].set_title('Amount spent per month')
 axs[1].set_xticklabels(grouped_transactions_month.index.strftime('%Y-%b'))
+
+
+
+
+annot = axs[0].annotate("", xy=(0,0), xytext=(-20,20),textcoords="offset points",
+                    bbox=dict(boxstyle="round", fc="w"),
+                    arrowprops=dict(arrowstyle="->"))
+annot.set_visible(False)
+
+def update_annot(ind):
+    horizontal_pos_array = ind["ind"] #Where the hover event takes place (normalised horizontal position)
+    x,y = line.get_data()
+    annot.xy = (x[horizontal_pos_array[0]], y[horizontal_pos_array[0]]) #Using minimum value for consistency (if hovering over more than one)
+    amount_text = graph_helpers.format_currency(y[horizontal_pos_array.min()])
+    date_text = x[horizontal_pos_array.min()]
+    text = f'{date_text}, £{amount_text}'
+    annot.set_text(text)
+    annot.get_bbox_patch().set_alpha(0.4)
+
+def hover(event):
+    vis = annot.get_visible()
+    if event.inaxes == axs[0]:
+        cont, ind = line.contains(event)
+        if cont:
+            update_annot(ind)
+            annot.set_visible(True)
+            fig.canvas.draw_idle()
+        else:
+            if vis:
+                annot.set_visible(False)
+                fig.canvas.draw_idle()
+
+fig.canvas.mpl_connect("motion_notify_event", hover)
 
 plt.show()
